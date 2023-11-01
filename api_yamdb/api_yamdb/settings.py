2f300b66ce9ce3a09f602dda5a2e10c53631e7ef
Егор Крыльцов <kryltsof@yandex.ru>
2023-11-01 21:12:45 +0300

    Fix urls.

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'user.apps.UserConfig',
     'api.apps.ApiConfig',
     'rest_framework',
-    'django-filters',
+    'django_filters',
     'reviews.apps.ReviewsConfig',
 ]
 
